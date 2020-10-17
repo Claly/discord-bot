36abee7b303f034165625a45ff463f8468940ef4
Talha Şöhret <46840264+Fenish@users.noreply.github.com>
2020-10-17 15:43:50 +0300

    Update main.py
    removed unnecessary comment lines

--- main.py
+++ main.py
@@ -12,25 +12,10 @@ yaml = YAML()
 
 bot.keydata = sqlite3.connect(f"userkeys.db")
 bot.keydata.execute("CREATE TABLE IF NOT EXISTS nightapi (id INTEGER PRIMARY KEY AUTOINCREMENT, discord_id VARCHAR(255), api_key VARCHAR(255))")
-
-
 bot.keys = bot.keydata.execute("SELECT * FROM nightapi").fetchall()
 with open("config.yml", "r") as stream:
 	bot.config = yaml.load(stream) 
-
-
 bot.remove_command("help")
-#for cog in [f for f in listdir("cogs") if isfile(join("cogs", f))]:
-#	if cog.endswith(".py"):
-#		try:
-#			cog = f"cogs.{cog[0:-3]}"
-#			bot.load_extension(cog)
-#			print(f"Added - {cog[5:].capitalize()}")
-#		except Exception as e:
-#			print(f"Can't load {cog}: {e}")
-
-
-
 async def console():
     cmd = await ainput("\n>>> ")
     os.system("clear")
@@ -67,23 +52,9 @@ async def setup():
                 print(f'[-] {cog}')
                 raise e
 
-#async def status_task():
-#	while True:
-#		response = requests.get("https://api.nightservers.xyz/server/servers.php")
-#		cevur2 = response.json()
-#		await bot.change_presence(activity=discord.Game(name=f"{cevur2['totalserver']} sunucu"))
-#		await asyncio.sleep(10)
-#		await bot.change_presence(activity=discord.Game(name=f"{cevur2['totaluser']} müşteri"))
-#		await asyncio.sleep(10)
-
 @bot.event
 async def on_ready():
-	
 	await setup()
 	await console()
-#	response = requests.get("https://api.nightservers.xyz/server/servers.php")
-#	cevur2 = response.json()
-#	await bot.change_presence(activity=discord.Game(name=f"{cevur2['totalserver']} sunucu"))
-#	bot.loop.create_task(status_task())
 
 bot.run(bot.config["token"])
